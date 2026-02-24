ccaca201e0d609bc1100aa0ed777f0333ed1730b
Nishantsharma9720 <ns62771@gmail.com>
2026-02-24 10:51:02 +0530

    question 1

---
+++ Python Question/Question 1.py
@@ -0,0 +1,9 @@
+#Swap two numbers without using a third variable.
+a = 5
+b = 10                      
+
+a = a + b
+b = a - b
+a = a - b                           
+
+print("After swapping: a =", a, "b =", b)       
